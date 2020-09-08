f0b4888e540d45ef8f5ac804c00139600ba11b39
Rafael Ángel <49995103+musicollins@users.noreply.github.com>
2020-09-08 19:29:22 +0200

    Project Skeleton 1.1
    SpringBoot skeleton created
    folder structure created
    application.properties configured
    java --v  =>  java 1.8
    dependencies => see pom.xml
    
    see application.properties file for more info about the database name.

---
+++ holidaymaker/src/main/java/com/newton/holidaymaker/HolidaymakerApplication.java
@@ -0,0 +1,15 @@
+package com.newton.holidaymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaymakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaymakerApplication.class, args);
+	}
+
+	
+
+}
