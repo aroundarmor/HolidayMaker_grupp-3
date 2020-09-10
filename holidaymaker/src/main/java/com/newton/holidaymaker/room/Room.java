e2e20192714e34b9e8629873e41040433e923153
reddington12 <37213559+Reddington12@users.noreply.github.com>
2020-09-10 23:27:06 +0200

    Commit created Room entity and a controller class

---
+++ holidaymaker/src/main/java/com/newton/holidaymaker/room/Room.java
@@ -0,0 +1,45 @@
+package com.newton.holidaymaker.room;
+
+public class Room {
+	private final long roomId;
+	private final String roomType;
+	private final long roomPrice;
+	private final long hotelId;
+	private final long bookingId;
+	private final boolean isBooked;
+
+	public Room(long roomId, String roomType, long roomPrice, long hotelId, long bookingId, boolean isBooked) {
+		super();
+		this.roomId = roomId;
+		this.roomType = roomType;
+		this.roomPrice = roomPrice;
+		this.hotelId = hotelId;
+		this.bookingId = bookingId;
+		this.isBooked = isBooked;
+	}
+
+	public long getRoomId() {
+		return roomId;
+	}
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+	public long getRoomPrice() {
+		return roomPrice;
+	}
+
+	public long getHotelId() {
+		return hotelId;
+	}
+
+	public long getBookingId() {
+		return bookingId;
+	}
+
+	public boolean isBooked() {
+		return isBooked;
+	}
+
+}
---
+++ holidaymaker/src/main/java/com/newton/holidaymaker/room/RoomController.java
@@ -0,0 +1,10 @@
+package com.newton.holidaymaker.room;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/here/room")
+public class RoomController {
+	
+
+}
