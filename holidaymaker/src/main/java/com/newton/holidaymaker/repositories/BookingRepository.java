55080cfd9f7bed3f4da4ffda0ba91566b4189e36
hmpettersson <62103380+hmpettersson@users.noreply.github.com>
2020-09-14 11:54:51 +0200

    Delete BookingRepository.java
    Tog bort filen för att kunna ladda upp ny utan konflikter

--- holidaymaker/src/main/java/com/newton/holidaymaker/repositories/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.newton.holidaymaker.repositories;
-import com.newton.holidaymaker.models.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookingRepository extends JpaRepository <Booking, Integer> {
-    
-}
