f087c72d815eb64257377e2c4890920e1d6f2e6b
reddington12 <37213559+Reddington12@users.noreply.github.com>
2020-09-14 23:28:27 +0200

    // removed service class and made changes in controller

--- holidaymaker/src/main/java/com/newton/holidaymaker/controllers/RoomController.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/controllers/RoomController.java
@@ -1,26 +1,41 @@
 package com.newton.holidaymaker.controllers;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newton.holidaymaker.models.Room;
-import com.newton.holidaymaker.service.RoomService;
+import com.newton.holidaymaker.repositories.RoomRepository;
+
+
+
 
 @RestController
+@Service
 public class RoomController {
 	
-@Autowired RoomService roomService;
+@Autowired RoomRepository roomRepository;
 	
 	
-	@RequestMapping("/rooms")
-	public List <Room> getAllRooms() {
-		return roomService.getAllRooms();
-	}
 
 		
+		
+	
+		
+@GetMapping("/rooms")
+public List<Room> getAllRooms(){
+return roomRepository.findAll();
+
+	
+   
 }
 
 
-//git problem
+
+
+}
