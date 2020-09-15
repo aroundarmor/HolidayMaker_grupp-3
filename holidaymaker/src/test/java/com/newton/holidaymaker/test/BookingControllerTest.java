3275cc26f0efb955c74e30b0b9a4a25122e41290
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-15 09:49:13 +0200

    sample test classes

---
+++ holidaymaker/src/test/java/com/newton/holidaymaker/test/BookingControllerTest.java
@@ -0,0 +1,46 @@
+package com.newton.holidaymaker.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.newton.holidaymaker.controllers.BookingController;
+import com.newton.holidaymaker.repositories.BookingRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class BookingControllerTest {
+
+	@Mock
+    private BookingRepository repository;
+	
+	@InjectMocks
+    private BookingController bookingController;
+
+    @BeforeEach
+    void init() {
+
+    }
+
+    @Test
+    void testGetBookings() {
+        System.out.println("Test: /getBookings/");
+    }
+
+    @Test
+    void testSaveBooking() {
+        System.out.println("Test: /saveBookings/{id}");
+    }
+
+    @Test
+    void testUpdateBooking() {
+        System.out.println("Test: /updateBooking/{id}");
+    }
+
+    @Test
+    void testDeleteBooking() {
+        System.out.println("Test: /deleteBooking/{id}");
+    }
+}
--- holidaymaker/src/test/java/com/newton/holidaymaker/test/HolidaymakerApplicationTests.java
+++ holidaymaker/src/test/java/com/newton/holidaymaker/test/HolidaymakerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.newton.holidaymaker;
+package com.newton.holidaymaker.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
