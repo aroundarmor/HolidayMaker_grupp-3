11d7e139301bec0296a60510f0fc6e700a6568f3
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-15 06:03:51 +0200

    Changes (#18)
    * remove extra lines
    
    * Tidy up
    
    * column error fix
    
    * add repository annotations

--- holidaymaker/src/main/java/com/newton/holidaymaker/controllers/MainController.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/controllers/MainController.java
@@ -1,22 +1,17 @@
 package com.newton.holidaymaker.controllers;
 
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    
     @GetMapping("/")
     public String homePage(){
         return "Welcome User: Home Page";
     }
+
     @GetMapping("/admin")
     public String adminPage(){
         return "Welcome Admin: Admin Page";
     }
-
-    
-        
 }
--- holidaymaker/src/main/java/com/newton/holidaymaker/repositories/UserRepository.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.newton.holidaymaker.repositories;
 import com.newton.holidaymaker.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     
 }
