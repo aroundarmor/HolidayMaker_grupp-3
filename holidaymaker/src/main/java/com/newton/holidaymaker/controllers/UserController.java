6a5fb123d4622a503c0e1bf2830bb4b163d9e9c2
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-15 04:22:05 +0200

    Tidy up

--- holidaymaker/src/main/java/com/newton/holidaymaker/controllers/UserController.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/controllers/UserController.java
@@ -11,16 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
     @Autowired
     private UserRepository userRepository;
-    
+
     @GetMapping("/")
     public List<User> allUsers(){
         return userRepository.findAll();
@@ -28,11 +24,6 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Integer id) {
-
         return userRepository.findById(id).get();
     }
-    
-
-    
-    
 }
