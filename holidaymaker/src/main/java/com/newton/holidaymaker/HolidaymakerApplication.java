9f56143f93288612b75966bcd20678bdc812f2f5
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-16 11:57:41 +0200

    Initial thymeleaf commit (#24)

--- holidaymaker/src/main/java/com/newton/holidaymaker/HolidaymakerApplication.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/HolidaymakerApplication.java
@@ -2,8 +2,10 @@ package com.newton.holidaymaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.newton.holidaymaker")
 public class HolidaymakerApplication {
 
 	public static void main(String[] args) {
