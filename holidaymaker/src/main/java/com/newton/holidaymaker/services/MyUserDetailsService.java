ee8add4142b6c703a478d397c12c72734464c131
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-19 10:17:11 +0200

    add annotation

--- holidaymaker/src/main/java/com/newton/holidaymaker/services/MyUserDetailsService.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/services/MyUserDetailsService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    
+    @Autowired
     private UserRepository userRepository;
 
     public MyUserDetailsService(UserRepository userRepository){
