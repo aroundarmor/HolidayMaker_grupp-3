9242376d8ca9fcd25c244664e4472f9232333df4
Ted Dahlqvist <teddahlqvist93@gmail.com>
2020-09-19 13:59:45 +0200

    Hotel Room relations
    Condenserade även getters och setters i enlighet med "User", och gjorde både Hotel och Room mer enhetliga med Booking/User

--- holidaymaker/src/main/java/com/newton/holidaymaker/models/Room.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/models/Room.java
@@ -5,73 +5,49 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="room")
 public class Room {
-    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)                private int id;
-/*    @Column(name="roomId")												    private int roomId;*/
-    @Column(name="roomType")    											private String roomType;
-    @Column(name="roomPrice")    											private double roomPrice;
-    @Column(name="hotelId")     											private int hotelId;
-    /*@Column(name="bookingId")   											private int bookingId;*/
-    @Column(name="isBooked")            									private Boolean isBooked;
-
-	public Room() { }
-	public Room(String roomType, double roomPrice, int hotelId,/*int bookingId, */ boolean isBooked) {
-		this.roomType = roomType;
-		this.roomPrice = roomPrice;
-		this.hotelId = hotelId;
-		//this.bookingId = bookingId;
-		this.isBooked = isBooked;
-	}
+        @Id @GeneratedValue (strategy = GenerationType.IDENTITY)        private int roomId;
+        @Column(name="roomType")                                        private String roomType;
+        @Column(name="roomPrice")    				    private double roomPrice;
+        @Column(name="hotelId")     				    private int hotelId;
+        @Column(name="isBooked")            			    private Boolean isBooked;
+        /*@Column(name="bookingId")   				    private int bookingId;
+        @Column(name="roomId")					    private int roomId;*/
+    
+        @ManyToOne
+        @JoinColumn(name = "hotelId", insertable = false, updatable = false)
+        private Hotel hotels;
+    
+        public Room() { }
+        public Room(String roomType, double roomPrice, int hotelId,/*int bookingId, */ boolean isBooked) {
+                this.roomType = roomType;
+                this.roomPrice = roomPrice;
+                this.hotelId = hotelId;
+                this.isBooked = isBooked;
+             //this.bookingId = bookingId;
+        }
+        
+        public Boolean getIsBooked(){return isBooked;}
+        public Hotel getHotels() {return hotels;}
+        public int getRoomId() {return roomId;}
+        public String getRoomType() {return roomType;}
+        public double getRoomPrice() {return roomPrice;}
+        public int getHotelId() {return hotelId;}
+        
+        public void setRoomType(String roomType) {this.roomType = roomType;}
+        public void setRoomPrice(double roomPrice) {this.roomPrice = roomPrice;}
+        public void setHotelId(int hotelId) {this.hotelId = hotelId;}
+        public boolean isBooked() {return isBooked;}
+        public void setBooked(boolean isBooked) {this.isBooked = isBooked;}
 /*
-	public int getRoomId() {
-		return roomId;
-	}
-
-	public void setRoomId(int roomId) {
-		this.roomId = roomId;
-	}
-*/
-	public String getRoomType() {
-		return roomType;
-	}
-
-	public void setRoomType(String roomType) {
-		this.roomType = roomType;
-	}
-
-	public double getRoomPrice() {
-		return roomPrice;
-	}
-
-	public void setRoomPrice(double roomPrice) {
-		this.roomPrice = roomPrice;
-	}
-
-	public int getHotelId() {
-		return hotelId;
-	}
-
-	public void setHotelId(int hotelId) {
-		this.hotelId = hotelId;
-	}
-/*
-	public int getBookingId() {
-		return bookingId;
-	}
-
-	public void setBookingId(int bookingId) {
-		this.bookingId = bookingId;
-	}
-*/
-	public boolean isBooked() {
-		return isBooked;
-	}
-
-	public void setBooked(boolean isBooked) {
-		this.isBooked = isBooked;
-	}
+       public void setRoomId(int roomId) {this.roomId = roomId;}
+        public int getBookingId() {return bookingId;
+       public void setBookingId(int bookingId) {this.bookingId = bookingId;}
+*/    
 }
