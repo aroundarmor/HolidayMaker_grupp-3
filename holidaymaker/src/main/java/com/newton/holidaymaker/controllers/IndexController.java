59055650ea77be92491fc572d552531476b851c9
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-20 17:30:31 +0200

    retrieve country names

--- holidaymaker/src/main/java/com/newton/holidaymaker/controllers/IndexController.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/controllers/IndexController.java
@@ -5,16 +5,26 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.newton.holidaymaker.repositories.HotelRepository;
+
 @Controller
 public class IndexController extends PageControllerEssentials implements PageControllerInterface {
 
+	@Autowired
+	HotelRepository hotelRepository;
+	
     @GetMapping("/")
     public ModelAndView run(HttpServletRequest req, HttpServletResponse res, Principal principal) {
         ModelAndView mv = initModelAndView("HolidayMaker | Welcome", null, "main");
+        
+        // Retrieve all country names form Hotel repo.
+        // This will be used to display hotel-countries that are available in the database.
+        mv.addObject("availableCountries", hotelRepository.findAllCountries());
 
         if(principal != null)
             mv.addObject("username", principal.getName());
--- holidaymaker/src/main/java/com/newton/holidaymaker/repositories/HotelRepository.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/repositories/HotelRepository.java
@@ -9,6 +9,7 @@ import com.newton.holidaymaker.models.Hotel;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -19,4 +20,7 @@ import org.springframework.stereotype.Repository;
 public interface HotelRepository extends JpaRepository<Hotel, Integer>{
 	List<Hotel> findByHotelCountry(String hotelCountry);
 	List<Hotel> findByHotelName(String hotelName);
+	
+	@Query(value="SELECT DISTINCT hotelCountry FROM Hotel")
+	List<String> findAllCountries();
 }
