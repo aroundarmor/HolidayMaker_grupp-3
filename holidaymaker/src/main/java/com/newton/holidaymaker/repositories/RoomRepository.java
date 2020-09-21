20e203a8ae703a5bd3171d764b93ae5b3ca5ba8b
idontreallywolf <nedim.kanat1999@gmail.com>
2020-09-21 11:47:23 +0200

    add method to room repository to retrieve roomTypes

--- holidaymaker/src/main/java/com/newton/holidaymaker/repositories/RoomRepository.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/repositories/RoomRepository.java
@@ -1,12 +1,16 @@
 package com.newton.holidaymaker.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.newton.holidaymaker.models.Room;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer>{
-
+    @Query("SELECT DISTINCT roomType FROM Room")
+    List<String> findAllRoomTypes();
 }
-//git problem
