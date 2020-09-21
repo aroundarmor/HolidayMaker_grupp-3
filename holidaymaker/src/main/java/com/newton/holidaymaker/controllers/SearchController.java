011e4c0ca5317b42a01c6f4d558d6a46c44c6786
Rafael Ángel <49995103+musicollins@users.noreply.github.com>
2020-09-21 10:22:25 +0200

    Read Description of changes
    RoomRepository queries for filtering
    SearchController created to filter user search, endpoints created.

---
+++ holidaymaker/src/main/java/com/newton/holidaymaker/controllers/SearchController.java
@@ -0,0 +1,59 @@
+package com.newton.holidaymaker.controllers;
+
+import java.util.List;
+
+import com.newton.holidaymaker.models.Hotel;
+import com.newton.holidaymaker.models.Room;
+import com.newton.holidaymaker.repositories.HotelRepository;
+import com.newton.holidaymaker.repositories.RoomRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    
+    
+    @Autowired
+    private HotelRepository hotelRepo;
+    @Autowired
+    private RoomRepository roomRepo;
+    
+    @GetMapping("/{country}")
+    public List<Hotel> findByCountry (@PathVariable String country){
+        return hotelRepo.findByHotelCountry(country);
+    }
+
+    @GetMapping("/price-asc")
+    public List<Room> findByPriceAsc(){
+        return roomRepo.findAllByOrderByRoomPriceAsc();
+    }
+    @GetMapping("/price-desc")
+    public List<Room> findByPriceDesc(){
+        return roomRepo.findAllByOrderByRoomPriceDesc();
+    }
+
+    @GetMapping("/available")
+    public List<Room> availableRooms(){
+        return roomRepo.findAllByIsBookedTrue();
+    }
+    // @GetMapping("/available/price-asc")
+    // public List<Room> availableRoomsByPriceAsc(){
+    //     return roomRepo.findAllByIsBookedFalseOrderByRoomPriceAsc();
+    // }
+    @GetMapping("/available/price-asc")
+    public List<Room> availableRoomsByPriceAsc(){
+        return roomRepo.findAllByIsBookedTrueOrderByRoomPriceAsc();
+    }
+    @GetMapping("/available/price-desc")
+    public List<Room> availableRoomsByPriceDesc(){
+        return roomRepo.findAllByIsBookedTrueOrderByRoomPriceDesc();
+    }
+    
+
+}
--- holidaymaker/src/main/java/com/newton/holidaymaker/repositories/RoomRepository.java
+++ holidaymaker/src/main/java/com/newton/holidaymaker/repositories/RoomRepository.java
@@ -1,7 +1,7 @@
 package com.newton.holidaymaker.repositories;
 
-import com.newton.holidaymaker.models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.newton.holidaymaker.models.Room;
@@ -12,6 +12,12 @@ public interface RoomRepository extends JpaRepository<Room, Integer>{
 	List<Room> getRoomsByHotelId(int getRoomsByHotelId);
 	List<Room> getRoomsByRoomType(String getRoomsByRoomType);
 	List<Room> getRoomsByIsBooked(Boolean getRoomsByIsBooked);
+	List<Room> findAllByOrderByRoomPriceAsc();
+	List<Room> findAllByOrderByRoomPriceDesc();
+	List<Room> findAllByIsBookedTrue();
+	List<Room> findAllByIsBookedTrueOrderByRoomPriceAsc();
+	List<Room> findAllByIsBookedTrueOrderByRoomPriceDesc();
+
 
 }
 //git problem
